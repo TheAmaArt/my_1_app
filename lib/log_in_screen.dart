95869b20103c8f01535dad0c0a4cf2915e763062
Amedeo De Marco <ama@LaptopvonAmedeo.fritz.box>
2023-08-08 18:48:40 +0200

    second Page with background color and BottomNavigationBar

--- lib/log_in_screen.dart
+++ lib/log_in_screen.dart
@@ -1,18 +1,66 @@
 import "package:flutter/material.dart";
+
 class LogInPage extends StatelessWidget {
   const LogInPage({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Log In"),
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: <Color>[Colors.pinkAccent, Colors.yellow],
+            ),
+          ),
+        ),
+        title: const Text(
+          "Log In",
+          style: TextStyle(color: Colors.black),
+        ),
       ),
       body: Center(
-        child: FloatingActionButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
+        child: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              stops: [
+                0.1,
+                0.4,
+                0.6,
+                0.9,
+              ],
+              colors: [
+                Colors.yellow,
+                Colors.red,
+                Colors.indigo,
+                Colors.teal,
+              ],
+            ),
+          ),
+          // // child: FloatingActionButton(
+          // //   onPressed: () {
+          // //     Navigator.pop(context);
+          //   },
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.yellow,
+        items: const [
+          BottomNavigationBarItem(
+            label: "Home",
+            icon: Icon(
+              Icons.home,
+              color: Colors.black,
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: "Play",
+            icon: Icon(Icons.gamepad, color: Colors.black),
+          ),
+        ],
       ),
     );
   }
